fa4771bb5c72e96bd4811b78bf84d16aec80e48e
William Bérubé <wberube@null.net>
2025-04-15 04:04:18 +0000

    Generic thermal zone sensors as fallback

--- src/hal/support.c
+++ src/hal/support.c
@@ -342,23 +342,22 @@ float hal_temperature_read(void) {
             }
             break;
         }
-        case HAL_PLATFORM_RK:
-        {
-            FILE* file;
-            char line[10] = {0};
-            if (file = fopen("/sys/class/thermal/thermal_zone0/temp", "r")) {
-                fgets(line, 10, file);
-                lastReadTemp = strtof(line, NULL) / 1000.0;
-                fclose(file);
-            }
-            break;
-        }
 #elif defined(__ARM_PCS)
         case HAL_PLATFORM_V2: lastReadTemp = v2_system_readtemp(); break;
         case HAL_PLATFORM_V3: lastReadTemp = v3_system_readtemp(); break;
         case HAL_PLATFORM_V4: lastReadTemp = v4_system_readtemp(); break;
 #endif
-        default: lastMillisTemp = UINT64_MAX; break;
+        default:
+            if (!access("/sys/class/thermal/thermal_zone0/temp", F_OK)) {
+                FILE* file;
+                char line[10] = {0};
+                if (file = fopen("/sys/class/thermal/thermal_zone0/temp", "r")) {
+                    fgets(line, 10, file);
+                    lastReadTemp = strtof(line, NULL) / 1000.0;
+                    fclose(file);
+                }
+            } else lastMillisTemp = UINT64_MAX;
+            break;
     }
 
     return lastReadTemp;
