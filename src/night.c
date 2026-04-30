088cf323fbc0425e197d2640bc3b01223cfb6549
William Bérubé <wberube@null.net>
2026-04-30 00:27:07 +0000

    Resolving IR sensor bypass in the night module

--- src/night.c
+++ src/night.c
@@ -76,11 +76,9 @@ void *night_thread(void) {
     } else {
         while (keepRunning) {
             bool state = false;
-            if (!gpio_read(app_config.ir_sensor_pin, &state)) {
-                sleep(app_config.check_interval_s);
-                continue;
-            }
-            if (!manual) night_mode(night_mode);
+            if (!gpio_read(app_config.ir_sensor_pin, &state))
+                if (!manual) night_mode(state);
+
             sleep(app_config.check_interval_s);
         }
     }
